f83c9baa511c26d6e844377019be983bff52428d
KirmesBude <kirmesbude@gmail.com>
2026-01-29 20:52:18 +0100

    Add object and player start to draw_grid

--- sector_shift_editor/src/systems/draw_grid.rs
+++ sector_shift_editor/src/systems/draw_grid.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::*;
-use sector_shift_core::prelude::TileType;
+use sector_shift_core::{maps::MapObject, prelude::TileType};
 
 use crate::{CELL_SIZE, HALF_CELL_SIZE, resources::MapData};
 
@@ -28,7 +28,7 @@ pub fn draw_grid(mut gizmos: Gizmos, editor: Res<MapData>) {
         );
     }
 
-    // Draw filled boxes
+    // Draw tiles
     for (position, tile) in editor.level.tiles.enumerate() {
         if *tile == TileType::Floor {
             continue;
@@ -41,4 +41,41 @@ pub fn draw_grid(mut gizmos: Gizmos, editor: Res<MapData>) {
 
         gizmos.rect_2d(center, Vec2::splat(CELL_SIZE * 0.9), tile.color());
     }
+
+    // Draw objects
+    for (position, object) in editor.level.objects.iter() {
+        let center = Vec2::new(
+            position.0 as f32 * CELL_SIZE + HALF_CELL_SIZE,
+            position.1 as f32 * CELL_SIZE + HALF_CELL_SIZE,
+        );
+
+        match object {
+            MapObject::Exit(_) => {
+                gizmos.cross_2d(center, HALF_CELL_SIZE * 0.8, Color::srgb(0.1, 0.4, 0.8));
+            },
+            MapObject::Enemy(_) => {
+                let coord = Vec2::new(HALF_CELL_SIZE * 0.8, HALF_CELL_SIZE * 0.8);
+                let start = center - coord;
+                let end = center + coord;
+                gizmos.line_2d(start, end, Color::srgb(0.8, 0.1, 0.1));
+            },
+            MapObject::Item(_) => {
+                gizmos.circle_2d(center, HALF_CELL_SIZE * 0.8, Color::srgb(0.1, 0.8, 0.1));
+            },
+        }
+    }
+
+    // Draw player start
+    let (position, direction) = editor.level.player_start;
+    let center = Vec2::new(
+        position.0 as f32 * CELL_SIZE + HALF_CELL_SIZE,
+        position.1 as f32 * CELL_SIZE + HALF_CELL_SIZE,
+    );
+    let coord = Vec2::new(
+        direction.coord().0 as f32 * HALF_CELL_SIZE * 0.8,
+        direction.coord().1 as f32 * HALF_CELL_SIZE * 0.8,
+    );
+    let start = center - coord;
+    let end = center + coord;
+    gizmos.arrow_2d(start, end, Color::srgb(0.1, 0.8, 0.1));
 }
