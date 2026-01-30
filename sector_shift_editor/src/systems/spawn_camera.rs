4ffbf469d452d5bc8fa98fe91a65fefd961085e4
KirmesBude <kirmesbude@gmail.com>
2026-01-30 21:52:30 +0100

    Spawn editor camera in the middle

--- sector_shift_editor/src/systems/spawn_camera.rs
+++ sector_shift_editor/src/systems/spawn_camera.rs
@@ -3,5 +3,9 @@ use bevy::prelude::*;
 use crate::CELL_SIZE;
 
 pub fn spawn_camera(mut commands: Commands) {
-    commands.spawn((Camera2d, Transform::from_xyz(32.0/2.0*CELL_SIZE, 32.0/2.0*CELL_SIZE, 0.0)));
+    commands.spawn((
+        Camera2d,
+        // TODO: Do not hard code editor map size
+        Transform::from_xyz(32.0 / 2.0 * CELL_SIZE, 32.0 / 2.0 * CELL_SIZE, 0.0),
+    ));
 }
