e26fabf6a4984dfd4073a7a0ffb87a9224165518
KirmesBude <kirmesbude@gmail.com>
2026-01-30 21:51:56 +0100

    Spawn item proper, rename medkit

--- sector_shift_game/src/items/functions/spawn_item.rs
+++ sector_shift_game/src/items/functions/spawn_item.rs
@@ -16,19 +16,24 @@ pub fn spawn_item(
     item_library: &ItemLibrary,
     item_id: &str,
     transform: Transform,
+    item_mesh: Handle<Mesh>,
+    materials: &mut Assets<StandardMaterial>,
 ) -> Option<Entity> {
     if let Some(item_definition) = item_library.get(item_id) {
+        // TODO: Move this out so it is only created once?
+        let material = materials.add(StandardMaterial {
+            base_color_texture: Some(item_definition.sprite.clone()),
+            cull_mode: None, // TODO: Rectangle has texture on -z? Is there a better way?
+            ..Default::default()
+        });
         let entity = commands
             .spawn((
                 Name::new(item_definition.id.clone()),
                 Item {
                     effect: item_definition.effect.clone(),
                 },
-                Sprite {
-                    image: item_definition.sprite.clone(),
-                    custom_size: Some(Vec2::new(1.0, 1.0)), // Match collider
-                    ..Default::default()
-                },
+                Mesh3d(item_mesh),
+                MeshMaterial3d(material),
                 transform,
                 Billboard,
                 Collider::cylinder(0.5, 1.0), // Match sprite size
